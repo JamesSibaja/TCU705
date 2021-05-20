3840af879fefde3b5a33cb4cd104f0c1d05e2bd7
jfsibajag <jsibajagranados2@gmail.com>
2021-05-20 15:24:49 -0600

    visualisar lista de datos

--- main.py
+++ main.py
@@ -10,45 +10,79 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
 from kivy.lang import Builder
+from kivy.uix.relativelayout import RelativeLayout
+#from kivymd.uix.textfield import MDTextField
 
 Builder.load_file('design.kv')
 
+class Pantalla(BoxLayout):
+    def __init__(self):
+        super(Pantalla, self).__init__()
+        self.add_widget(Barra())
+        self.add_widget(ListaBase(['Nombre', 'Telefono','Carrera']))
 
-def read_from_db(entrada):
-    c.execute('SELECT * FROM CV')
-    cont = 0
-    text = ''
-    data = c.fetchall()
-    #print(data)
-    for row in data:
-        cont+=1
-        text += str(row)
-        if cont == entrada:
-            break
-    return text
+class Barra(BoxLayout):
+    def __init__(self):
+        super(Barra, self).__init__()
+        #self.username = MDTextField(text="hola")
 
+class ListaBase(BoxLayout):
+    def __init__(self,entrada):
+        super(ListaBase, self).__init__()
+        for row in entrada:
+            self.add_widget(MyWidget(row))
 
 class MyWidget(BoxLayout):
     
     def __init__(self,entrada):
         super(MyWidget, self).__init__()
-        for x in range(0,entrada):
-            self.add_widget(campoBD())
+        color = True
+        cont = 0
+        texto = ''
+        self.add_widget(campoTitulo(entrada))
+        for row in c.execute("SELECT %s FROM CV" %entrada):
+            for x in row:
+                texto += x 
+                if color:
+                    
+                    self.add_widget(campoBD1(texto))
+                    color = False
+                    
+                else:
+                    self.add_widget(campoBD2(texto))
+                    color = True
+                
+            cont += 1
+            texto = ''
+            if cont == 15:
+                break
 
-class campoBD(BoxLayout):
+class campoTitulo(BoxLayout):
     g = StringProperty()
-    def __init__(self):
-        super(campoBD, self).__init__()
-        self.g = read_from_db(1)
+    def __init__(self,texto):
+        super(campoTitulo, self).__init__()
+        self.g = texto
+
+class campoBD1(BoxLayout):
+    g = StringProperty()
+    def __init__(self,texto):
+        super(campoBD1, self).__init__()
+        self.g = texto
+
+class campoBD2(BoxLayout):
+    g = StringProperty()
+    def __init__(self,texto):
+        super(campoBD2, self).__init__()
+        self.g = texto
 
 
 class myApp(App):
     title = 'Plataforma'
     def build(self):
-        return MyWidget(4)
+        return Pantalla()
     def on_pause(self):
         return True
-    def on_resume(self): #¿Para que sirve esta función?
+    def on_resume(self):
         pass
 
 if __name__ == '__main__':
