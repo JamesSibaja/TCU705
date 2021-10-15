db5805fab049d97b82c33513c5fe61aa9e58c712
Erick Sancho <alonso.sancho99@gmail.com>
2021-10-15 14:40:43 -0600

    Se agregó más funcionalidades a la logica de login, como por ejemplo el de registro de usuarios, la unificacion de las bases de datos, y formato de contraseña, ademas se agrego la página respectiva con las funcionalidades básicas de pagina de registro

--- principal/contrasenas.py
+++ principal/contrasenas.py
@@ -28,7 +28,7 @@ class passw_manager:
         rows = c.fetchall()
 
         for r in rows:
-            if r[0] == usuario and r[1] == password:
+            if r[0] == sha256(usuario.encode()).hexdigest() and r[1] == sha256(password.encode()).hexdigest():
                 self.data.commit()
                 self.data.close()
                 return True
@@ -39,13 +39,29 @@ class passw_manager:
 
         
         
+
     def save_new(self, usuario, password):
         self.data = sqlite3.connect(self.base)
 
         c = self.data.cursor()
 
-        c.execute('''INSERT INTO passwords
-             VALUES(?,?)''', (sha256(usuario.encode()).hexdigest(), sha256(password.encode()).hexdigest()))
+        c.execute('''SELECT * FROM passwords;''')
 
-        self.data.commit()
-        self.data.close()
+        rows = c.fetchall()
+
+        usuario_ya_añadido = False
+        for r in rows:
+            if r[0] == sha256(usuario.encode()).hexdigest():
+                usuario_ya_añadido = True
+                break
+
+        if (not usuario_ya_añadido):
+            c.execute('''INSERT INTO passwords
+                VALUES(?,?)''', (sha256(usuario.encode()).hexdigest(), sha256(password.encode()).hexdigest()))
+
+            self.data.commit()
+            self.data.close()
+            return True
+        else:
+            return False
+
--- principal/startMenu.py
+++ principal/startMenu.py
@@ -21,32 +21,39 @@ class StartMenu(BoxLayout):
         self.upApp = upApp
 
         #Aquí se define el menú inicial
-        self.menu = MenuInicial()
-        #self.menu.add_widget( startButton)
+        self.menu = MenuInicial(self.upApp)
 
-        self.menu.add_widget(Label(text="Nombre de usuario", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.7}))
-
-        self.user = TextInput(size_hint=(.4, .05), pos_hint={'x':.5, 'y':.7})
-        self.menu.add_widget(self.user)
-
-        self.menu.add_widget(Label(text="Contraseña", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.5}))
-        self.password = TextInput(password=True, size_hint=(.4, .05),  pos_hint={'x':.5, 'y':.5})
-        self.menu.add_widget(self.password)
-
-        self.submit = Button(text='Iniciar Seción', size_hint=(.3, .1), pos_hint={'x':.15, 'y':.2},on_press=self.loggin)
-        self.menu.add_widget(self.submit)
-
-        self.menu.add_widget( Button(text='Registrarse', size_hint=(.3, .1),
-                pos_hint={'x':.55, 'y':.2},on_press=self.loggin))
-        # self.menu.add_widget(Image(source='imagenes/logo.png', size_hint=(1, .6),
-        #         pos_hint={'x':0, 'y':0.35}))
-        #self.up = up
         self.add_widget(self.menu)
 
-        self.contrasenas_manager = passw_manager("pass.db")
 
-    # def build(self,obj):
-    #     self.up.build()
+# Clase que contiene el menu de inicio (pagina principal de la aplicacion)
+class MenuInicial(FloatLayout):
+    def __init__(self, upApp):
+        super().__init__()
+
+        self.upApp = upApp
+
+        self.build()
+
+        self.contrasenas_manager = passw_manager('base') # Se conecta al controlador de contrasenas por medio de un objeto diferente
+
+    def build(self, *args):
+        self.add_widget(Label(text="Nombre de usuario", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.7}))
+
+        self.user = TextInput(size_hint=(.4, .05), pos_hint={'x':.5, 'y':.7})
+        self.add_widget(self.user)
+
+        self.add_widget(Label(text="Contraseña", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.5}))
+        self.password = TextInput(password=True, size_hint=(.4, .05),  pos_hint={'x':.5, 'y':.5})
+        self.add_widget(self.password)
+
+        self.submit = Button(text='Iniciar Seción', size_hint=(.3, .1), pos_hint={'x':.15, 'y':.2},on_press=self.loggin)
+        self.add_widget(self.submit)
+
+        self.register = Button(text='Registrarse', size_hint=(.3, .1), pos_hint={'x':.55, 'y':.2},on_press=self.btn_register)
+        self.add_widget(self.register)
+
+
 
     def loggin(self,obj):
         if self.contrasenas_manager.comparar(self.user.text, self.password.text):
@@ -72,8 +79,118 @@ class StartMenu(BoxLayout):
 
         pop.open()
 
+    # Metodo del boton de registro
+    def btn_register(self, obj):
+        self.clear_widgets()
+        self.pag_register = RegisterWindow(self)
+        self.add_widget(self.pag_register)
 
 
 
-class MenuInicial(FloatLayout):
-    pass 
+
+# Clase que contiene la pagina de registro
+class RegisterWindow(FloatLayout):
+    def __init__(self, init_page):
+        super().__init__()
+
+        self.init_page = init_page
+        self.contrasenas_manager = passw_manager('base') # Se conecta al controlador de contrasenas por medio de un objeto diferente
+
+        self.add_widget(Label(text="Nombre de usuario", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.7}))
+
+        self.user = TextInput(size_hint=(.4, .05), pos_hint={'x':.5, 'y':.7})
+        self.add_widget(self.user)
+
+        self.add_widget(Label(text="Contraseña", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.5}))
+        self.password = TextInput(password=True, size_hint=(.4, .05),  pos_hint={'x':.5, 'y':.5})
+        self.add_widget(self.password)
+
+        self.add_widget(Label(text="Repetir Contraseña", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.4}))
+        self.password2 = TextInput(password=True, size_hint=(.4, .05),  pos_hint={'x':.5, 'y':.4})
+        self.add_widget(self.password2)
+
+        self.submit = Button(text='Registrarse', size_hint=(.3, .1), pos_hint={'x':.35, 'y':.2},on_press=self.btn_registrarse)
+        self.add_widget(self.submit)
+
+        self.btn_return = Button(size_hint = (0.1, 0.1) ,pos_hint={'x':.01, 'y':.9},background_normal = './imagenes/arrow.png', on_press=self.return_callback)
+        self.add_widget(self.btn_return)
+
+    # Metodo que se devuelve a la pagina de menu inicial
+    def return_callback(self,obj=None):
+        self.clear_widgets()
+        self.init_page.build(obj)
+        
+    # Metodo que realiza la operacion del registro de usuario y contraseña en la base de datos
+    def btn_registrarse(self,obj):
+        if(self.password.text == self.password2.text):
+            if (len(self.password.text)>=8 and len(self.user.text)>0):
+                
+
+                if(self.contrasenas_manager.save_new(self.user.text, self.password.text)):
+                    self.return_callback()
+
+                else:
+                    self.Fallo_UC()
+                    self.user.text=""
+                    self.password.text=""
+                    self.password2.text=""
+            else:
+                self.Fallo_longitud()
+                self.user.text=""
+                self.password.text=""
+                self.password2.text=""
+        else:
+            self.Fallo_passw_igual()
+            self.password.text=""
+            self.password2.text=""
+
+            
+
+    # En caso de haber un error en el usurio o contraseña se despliega un PopUp informando del error
+    def Fallo_UC(self):
+        content = Button(text='Aceptar', size_hint=(0.5, 0.5),font_size= 20)
+        pop = Popup(title='El nombre de usuario no se encuentra disponible',
+                content=content,
+                title_align = 'center',
+                title_size = '20',
+                auto_dismiss=False,
+                size_hint=(None, None), size=(350, 200))
+
+        content.bind(on_press=pop.dismiss)
+        
+        self.user.text = ""
+        self.password.text = ""
+
+        pop.open()
+    
+    # En caso de no cumplir con una contraseña de mas de 8 caracteres, se despliega un PopUp informando del error
+    def Fallo_longitud(self):
+        content = Button(text='Aceptar', size_hint=(0.5, 0.5),font_size= 20)
+        pop = Popup(title='La contraseña debe ser de más de 8 caracteres',
+                content=content,
+                title_align = 'center',
+                title_size = '20',
+                auto_dismiss=False,
+                size_hint=(None, None), size=(350, 200))
+
+        content.bind(on_press=pop.dismiss)
+        
+        self.user.text = ""
+        self.password.text = ""
+
+        pop.open()
+
+    # En caso de no cumplir con una contraseñas iguales, se despliega un PopUp informando del error
+    def Fallo_passw_igual(self):
+        content = Button(text='Aceptar', size_hint=(0.5, 0.5),font_size= 20)
+        pop = Popup(title='Las contraseñas no coinciden',
+                content=content,
+                title_align = 'center',
+                title_size = '20',
+                auto_dismiss=False,
+                size_hint=(None, None), size=(350, 200))
+
+        content.bind(on_press=pop.dismiss)
+
+        pop.open()
+
