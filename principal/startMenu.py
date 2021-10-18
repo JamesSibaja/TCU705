564a2d5b83d4ecb8a9a304e61064d54d8735429e
Erick Sancho <alonso.sancho99@gmail.com>
2021-10-17 22:51:30 -0600

    Se definieron nuevas limitaciones de nombre de usuario y de contraseña empleando expresiones regulares. Ademas se ajustaron los PopUps relacionados

--- principal/startMenu.py
+++ principal/startMenu.py
@@ -1,3 +1,5 @@
+from kivy.uix.behaviors import focus
+from kivy.uix.behaviors.focus import FocusBehavior
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image 
@@ -8,6 +10,7 @@ from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 
 from contrasenas import passw_manager
+import re
 
 # from kivy.config import Config
 # Config.set('kivy', 'keyboard_mode', 'system')
@@ -40,11 +43,11 @@ class MenuInicial(FloatLayout):
     def build(self, *args):
         self.add_widget(Label(text="Nombre de usuario", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.7}))
 
-        self.user = TextInput(size_hint=(.4, .05), pos_hint={'x':.5, 'y':.7})
+        self.user = TextInput(size_hint=(.4, .05), pos_hint={'x':.5, 'y':.7}, multiline=False, write_tab= False)
         self.add_widget(self.user)
 
         self.add_widget(Label(text="Contraseña", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.5}))
-        self.password = TextInput(password=True, size_hint=(.4, .05),  pos_hint={'x':.5, 'y':.5})
+        self.password = TextInput(password=True, size_hint=(.4, .05),  pos_hint={'x':.5, 'y':.5}, multiline=False, write_tab= False, on_text_validate=self.loggin)
         self.add_widget(self.password)
 
         self.submit = Button(text='Iniciar Seción', size_hint=(.3, .1), pos_hint={'x':.15, 'y':.2},on_press=self.loggin)
@@ -52,6 +55,13 @@ class MenuInicial(FloatLayout):
 
         self.register = Button(text='Registrarse', size_hint=(.3, .1), pos_hint={'x':.55, 'y':.2},on_press=self.btn_register)
         self.add_widget(self.register)
+
+        # Comandos empleados para la funcionalidad Focus, pasar entre inputs con el "TAB"
+        self.user.focus_next =self.password
+        self.password.focus_next = self.user
+
+        # self.user.set_next(self.password)
+        # self.password.set_next(self.user)
 
 
 
@@ -89,7 +99,7 @@ class MenuInicial(FloatLayout):
 
 
 # Clase que contiene la pagina de registro
-class RegisterWindow(FloatLayout):
+class RegisterWindow(FloatLayout, FocusBehavior):
     def __init__(self, init_page):
         super().__init__()
 
@@ -98,15 +108,15 @@ class RegisterWindow(FloatLayout):
 
         self.add_widget(Label(text="Nombre de usuario", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.7}))
 
-        self.user = TextInput(size_hint=(.4, .05), pos_hint={'x':.5, 'y':.7})
+        self.user = TextInput(size_hint=(.4, .05), pos_hint={'x':.5, 'y':.7}, multiline=False, write_tab= False)
         self.add_widget(self.user)
 
         self.add_widget(Label(text="Contraseña", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.5}))
-        self.password = TextInput(password=True, size_hint=(.4, .05),  pos_hint={'x':.5, 'y':.5})
+        self.password = TextInput(password=True, size_hint=(.4, .05),  pos_hint={'x':.5, 'y':.5}, multiline=False, write_tab= False)
         self.add_widget(self.password)
 
         self.add_widget(Label(text="Repetir Contraseña", size_hint=(.4, .05), pos_hint={'x':.15, 'y':.4}))
-        self.password2 = TextInput(password=True, size_hint=(.4, .05),  pos_hint={'x':.5, 'y':.4})
+        self.password2 = TextInput(password=True, size_hint=(.4, .05),  pos_hint={'x':.5, 'y':.4}, multiline=False, on_text_validate=self.btn_registrarse, write_tab= False)
         self.add_widget(self.password2)
 
         self.submit = Button(text='Registrarse', size_hint=(.3, .1), pos_hint={'x':.35, 'y':.2},on_press=self.btn_registrarse)
@@ -115,6 +125,10 @@ class RegisterWindow(FloatLayout):
         self.btn_return = Button(size_hint = (0.1, 0.1) ,pos_hint={'x':.01, 'y':.9},background_normal = './imagenes/arrow.png', on_press=self.return_callback)
         self.add_widget(self.btn_return)
 
+        self.user.focus_next = self.password
+        self.password.focus_next = self.password2
+        self.password2.focus_next = self.user
+
     # Metodo que se devuelve a la pagina de menu inicial
     def return_callback(self,obj=None):
         self.clear_widgets()
@@ -122,34 +136,40 @@ class RegisterWindow(FloatLayout):
         
     # Metodo que realiza la operacion del registro de usuario y contraseña en la base de datos
     def btn_registrarse(self,obj):
-        if(self.password.text == self.password2.text):
-            if (len(self.password.text)>=8 and len(self.user.text)>0):
-                
+        if (self.user.text == ""):
+            self.Fallo_UC()
+        
+        else:
+            if(self.password.text == self.password2.text):
 
-                if(self.contrasenas_manager.save_new(self.user.text, self.password.text)):
-                    self.return_callback()
+                if (re.findall("^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", self.password.text)):
+                    
+                    
+                    if(re.findall("^(?![-._])(?!.*[_.-]{2})[\w.-]{5,30}(?<![-._])$", self.user.text) and self.contrasenas_manager.save_new(self.user.text, self.password.text)):
+                        self.return_callback()
 
+                    else:
+                        self.Fallo_UC()
+                        self.user.text=""
+                        self.password.text=""
+                        self.password2.text=""
+                    
                 else:
-                    self.Fallo_UC()
+                    self.Fallo_longitud()
                     self.user.text=""
                     self.password.text=""
                     self.password2.text=""
             else:
-                self.Fallo_longitud()
-                self.user.text=""
+                self.Fallo_passw_igual()
                 self.password.text=""
                 self.password2.text=""
-        else:
-            self.Fallo_passw_igual()
-            self.password.text=""
-            self.password2.text=""
 
             
 
     # En caso de haber un error en el usurio o contraseña se despliega un PopUp informando del error
     def Fallo_UC(self):
         content = Button(text='Aceptar', size_hint=(0.5, 0.5),font_size= 20)
-        pop = Popup(title='El nombre de usuario no se encuentra disponible',
+        pop = Popup(title='El nombre de usuario no se encuentra disponible, o no es válido',
                 content=content,
                 title_align = 'center',
                 title_size = '20',
@@ -166,7 +186,7 @@ class RegisterWindow(FloatLayout):
     # En caso de no cumplir con una contraseña de mas de 8 caracteres, se despliega un PopUp informando del error
     def Fallo_longitud(self):
         content = Button(text='Aceptar', size_hint=(0.5, 0.5),font_size= 20)
-        pop = Popup(title='La contraseña debe ser de más de 8 caracteres',
+        pop = Popup(title='La contraseña debe tener mayúsculas, minúsculas, números y símbolos especiales',
                 content=content,
                 title_align = 'center',
                 title_size = '20',
